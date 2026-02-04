1554fff3621fdfcb461b35ce358ab148a659f1f4
emyengineer <emyengineer@gmail.com>
2026-02-04 16:23:18 +0200

    The First 5 Lessons in Angular Fundamentals Course 2026

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+// This file is no longer needed when using standalone components and bootstrapApplication.
+// You can safely delete this file.
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppComponent } from './app.component';
+// import { SiteHeaderComponent } from './site-header/site-header.component';
+// import { HomeComponent } from './home/home.component';
+// import { CatalogComponent } from './catalog/catalog.component';
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     BrowserModule,
+//     AppComponent,
+//     HomeComponent,
+//     CatalogComponent,
+//     SiteHeaderComponent
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
