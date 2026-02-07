11795019a6ed9a83167a4f6ac2cefb91873f63db
emyengineer <emyengineer@gmail.com>
2026-02-07 05:04:42 +0200

    Send a POST method

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 cart : IProduct[] = [];
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   add(product: IProduct) {
     this.cart.push(product);
+    this .http.post('/api/cart', product).subscribe({
+      next: (response) => console.log('Product added to cart on server:', response),
+      error: (error) => console.error('Error adding product to cart on server:', error)
+    });
     console.log(`Product ${product.name} added to cart. Cart now has ${this.cart.length} items.`);
   }
 
