ac77982807d7946d7c58511283ea2c0ca80e35f0
emyengineer <emyengineer@gmail.com>
2026-03-16 03:31:01 +0200

    Providing Better User Feedback from our Sign-in Events SignOut

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { IUser } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-site-header',
@@ -9,8 +11,26 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./site-header.component.css'],
   imports: [CommonModule, RouterModule]
 })
-export class SiteHeaderComponent {
+export class SiteHeaderComponent implements OnInit {
+  user : IUser | null = null;
+  showSignOutMenu: boolean = false;
+  constructor(private userService: UserService) { }
 
-  constructor() { }
+  ngOnInit() {
+      this.userService.getUser().
+      subscribe({next: (user) => {
+        this.user = user;
+      }});
+  }
+
+  toggleSignOutMenu() {
+    this.showSignOutMenu = !this.showSignOutMenu;
+  }
+
+  signOut() {
+    this.userService.signOut();
+    this.user = null;
+    this.showSignOutMenu = false;
+  }
 
 }
--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { IUserCredentials } from '../user.model';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-sign-in',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css'],
 })
@@ -17,9 +18,14 @@ export class SignInComponent {
     password: '',
   };
 
+  signInError: boolean = false;
+
+
   constructor(private userService: UserService, private router: Router) {}
 
   signIn() {
+    this.signInError = false;
+
     this.userService.signIn(this.credentials).subscribe({
       next: (user) => {
         console.log('User signed in:', user);
@@ -27,6 +33,7 @@ export class SignInComponent {
       },
       error: (error) => {
         console.error('Sign-in failed:', error);
+        this.signInError = true;
       },
     });
   }
